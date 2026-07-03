8f1e26ef11f0f4a7bfad2931a6debbfb6987946b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-07-03 10:54:14 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './components/home/home';
+import { Login } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
