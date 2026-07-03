d344d5b3f893522f04a846fae43b23854b2f0c58
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-07-03 14:58:32 +0530

    initial commit

--- src/app/directives/highlight.ts
+++ src/app/directives/highlight.ts
@@ -5,6 +5,7 @@ import { Directive, ElementRef, inject } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
+
 })
 export class HighlightDirective {
   private el = inject(ElementRef);
@@ -12,3 +13,37 @@ export class HighlightDirective {
     this.el.nativeElement.style.backgroundColor = 'yellow';
   }
 }
+
+
+
+// import { Directive, ElementRef, inject } from '@angular/core';
+
+// // On CMD run - 
+// // ng generate directive directives/highlight
+
+// @Directive({
+//   selector: '[appHighlight]',
+//   host: {
+//     '(mouseenter)': 'onMouseEnter()',
+//     '(mouseleave)': 'onMouseLeave()',
+//   },
+// })
+// export class HighlightDirective {
+
+//   private el = inject(ElementRef);
+
+//   constructor() {
+//     this.el.nativeElement.style.backgroundColor = 'yellow';
+//   }
+
+//   onMouseEnter() {
+//     this.highlight('blue');
+//   }
+  
+//   onMouseLeave() {
+//     this.highlight('orange');
+//   }
+//   private highlight(color: string) {
+//     this.el.nativeElement.style.backgroundColor = color;
+//   }
+// }
