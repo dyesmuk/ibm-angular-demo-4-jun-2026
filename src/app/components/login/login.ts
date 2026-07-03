5d4cf0c853dc669fc49cbbe696a2f17e956179fd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-07-03 14:50:13 +0530

    initial commit

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -10,20 +10,49 @@ import { FormsModule } from '@angular/forms';
 })
 export class Login {
 
-  username: string = '';
-  salary: number = 90000.50;
-  user = { email: 'sonu@ibm.com', password: 'sonu123' };
-  isIndian = false;
+  isSpecial = false;
+  currentStyles = {};
 
   fun = () => {
-    console.log('fun function called.');
-    this.isIndian = !this.isIndian;
+    this.isSpecial = !this.isSpecial;
+    this.currentStyles = { 'color': 'orange' };
   };
 
 
 
-
 }
+
+
+
+
+
+// import { CommonModule } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-login',
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './login.html',
+//   styleUrl: './login.css',
+// })
+// export class Login {
+
+//   username: string = '';
+//   salary: number = 90000.50;
+//   user = { email: 'sonu@ibm.com', password: 'sonu123' };
+//   isIndian = false;
+
+//   fun = () => {
+//     console.log('fun function called.');
+//     this.isIndian = !this.isIndian;
+//   };
+
+
+
+
+// }
+
 
 
 
