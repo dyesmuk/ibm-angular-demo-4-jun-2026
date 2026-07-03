632fe8aea681cbfe527ec2ed905c0b7484cc7b9f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-07-03 12:03:39 +0530

    initial commit

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
   username: string = 'Sonu';
+  salary: number = 90000.50;
 
   user = { email: 'sonu@ibm.com', password: 'sonu123' };
 
