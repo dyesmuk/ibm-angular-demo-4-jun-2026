97aff9f465b0cace9dd9756734139bb363d584f0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-07-06 15:54:16 +0530

    code updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
-
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { EmployeeList } from './components/employee-list/employee-list';
+import { Page404 } from './components/page404/page404';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
     { path: 'home', component: Home },
-    { path: 'employees', component: EmployeeList },
+    { path: 'employees', component: EmployeeList }, 
+    { path: '**', component: Page404 },
 ];
 
 
---
+++ src/app/components/page404/page404.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page404',
+  imports: [],
+  templateUrl: './page404.html',
+  styleUrl: './page404.css',
+})
+export class Page404 {}
